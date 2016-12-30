5ce6f50f22e19df93d2d035185f913b85cc4e07f
killian <killian@debian>
2016-12-30 13:01:30 +0000

    Added repl for user command inputs, need to link the commands to network actions next

--- client/main.go
+++ client/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+)
+
+func help(){
+	fmt.Println("Unknown command")
+}
+
+func list(_ string){
+	fmt.Println("Listing")
+}
+
 func main(){
 
+	funcs := make(map[string]func(string))
+
+	funcs["ls"] = list
+	
+	inp := ""
+	for {
+		fmt.Print(">")
+		fmt.Scanf("%s", &inp)
+		fmt.Println(inp)
+		command := funcs[inp]
+		if command == nil {
+			help()
+		} else {
+			command(inp)
+		}
+		
+	}
 }
