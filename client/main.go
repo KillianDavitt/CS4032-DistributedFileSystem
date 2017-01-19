f4f026bf4d58e3e642c80faa465e9ec8e66e542c
Killian Davitt <davittk@tcd.ie>
2017-01-19 15:44:50 +0000

    Added creation of config file for storing auth server info

--- client/main.go
+++ client/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"net"
+	"crypto/rsa"
 	"fmt"
+	"os"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 
 func help(){
@@ -28,10 +33,35 @@ func transaction_end(_ string){
 	fmt.Println("End transaction")
 }
 
+
+type authServer struct{
+	Ip net.IP
+	PubKey rsa.PublicKey
+}
+
+func getConfig(){
+	if _, err := os.Stat(".dfs.conf"); os.IsNotExist(err) {
+		newServ := &authServer{}
+		fmt.Println("Enter the ip of the auth server")
+		inp := ""
+		fmt.Scanf("%s", &inp)
+		newServ.Ip = net.ParseIP(inp)
+		authServBytes, err := bson.Marshal(newServ)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file, err := os.Create(".dfs.conf")
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Write(authServBytes)
+	}
+}
+
 func main(){
 
 	funcs := make(map[string]func(string))
-	
+	getConfig()
 	funcs["ls"] = list
 	funcs["put"] = put_file
 	funcs["transaction start"] = transaction_start
