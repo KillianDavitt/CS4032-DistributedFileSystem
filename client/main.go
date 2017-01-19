7d38b6d12acae31ce33e3d2fb72896b1f6c70189
Killian Davitt <davittk@tcd.ie>
2017-01-19 15:04:59 +0000

    Added readme to client, updated client cli

--- client/main.go
+++ client/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func help(){
-	fmt.Println("Unknown command")
+	fmt.Println("Help:\nls\nput\nget")
 }
 
 func list(_ string){
@@ -16,13 +16,31 @@ func put_file(_ string){
 	fmt.Println("Putting file")
 }
 
+func auth(_ string){
+	fmt.Println("Authing...")
+}
+
+func transaction_start(_ string){
+	fmt.Println("Starting transaction..")
+}
+
+func transaction_end(_ string){
+	fmt.Println("End transaction")
+}
+
 func main(){
 
 	funcs := make(map[string]func(string))
-
+	
 	funcs["ls"] = list
-	funcs["put"] = put_file	
+	funcs["put"] = put_file
+	funcs["transaction start"] = transaction_start
+	funcs["transaction end"] = transaction_end
 	inp := ""
+
+	fmt.Println("Welcome to DFS")
+	auth("hi")
+	help()
 	for {
 		fmt.Print(">")
 		fmt.Scanf("%s", &inp)
